cc64f8e8749874c4a8ad82a5cc1fefac0227cfd7
mbowe4 <mlbowe@gmail.com>
2018-04-16 00:21:04 -0400

    fixed

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -2,14 +2,18 @@ package io.zipcoder;
 
 class Problem1 {
 
-    public void wuTangPrinter() {
-        for(int i = 1; i <= 50; i++) {
+    public String wuTangPrinter(int maxNum) {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 1; i <= maxNum; i++) {
             if(toWuTangString(i).equals("")) {
-                System.out.println(i);
+                builder.append(i).append(", ");
+            } else if (i <= maxNum-1) {
+                builder.append(toWuTangString(i)).append(", ");
             } else {
-                System.out.println(toWuTangString(i));
+                builder.append(toWuTangString(i));
             }
         }
+        return builder.toString();
     }
 
     public String toWuTangString(int number) {
@@ -22,11 +26,6 @@ class Problem1 {
             result = "Tang";
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        Problem1 problem1 = new Problem1();
-        problem1.wuTangPrinter();
     }
 }
 
--- src/test/java/io/zipcoder/WuTangTest.java
+++ src/test/java/io/zipcoder/WuTangTest.java
@@ -29,10 +29,10 @@ public class WuTangTest {
     public void multipleOfThreeTest() {
         Problem1 problem1 = new Problem1();
 
-        String exptected = "Wu";
+        String expected = "Wu";
         String actual = problem1.toWuTangString(9);
 
-        Assert.assertEquals(exptected, actual);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
@@ -43,6 +43,18 @@ public class WuTangTest {
         String actual = problem1.toWuTangString(45);
 
         Assert.assertEquals(exptected, actual);
+    }
+
+    @Test
+    public void wuTangPrinterTest() {
+        Problem1 problem1 = new Problem1();
+        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, " +
+                "Wu, 13, 14, WuTang, 16, 17, Wu, 19, Tang, Wu, 22, 23, " +
+                "Wu, Tang, 26, Wu, 28, 29, WuTang, 31, 32, Wu, 34, Tang, Wu, " +
+                "37, 38, Wu, Tang, 41, Wu, 43, 44, WuTang, 46, 47, Wu, 49, Tang";
+        String actual = problem1.wuTangPrinter(50);
+
+        Assert.assertEquals(expected, actual);
     }
 
 
